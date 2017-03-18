2fc12fa25552d6575437a8a65c66e7c5d3569e6b
Kasper <Kasper@192.168.87.12>
2017-03-18 12:54:01 +0100

    Initial commit

---
+++ src/kdavi16/dm505/computerstore/presentation/StoreFrontController.java
@@ -0,0 +1,27 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kdavi16.dm505.computerstore.presentation;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.Initializable;
+
+/**
+ * FXML Controller class
+ *
+ * @author Kasper
+ */
+public class StoreFrontController implements Initializable {
+
+	/**
+	 * Initializes the controller class.
+	 */
+	@Override
+	public void initialize(URL url, ResourceBundle rb) {
+		// TODO
+	}	
+	
+}
